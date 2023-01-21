6afe472aa167608181a05f38a3772b1cab4f5e37
PralhadKadukar <you@example.com>
2023-01-21 19:32:57 +0530

    file added

---
+++ src/com/practice/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list = new ArrayList();
+		list.add("pranit");
+		list.add("pralhad");
+		System.out.println(list);
+
+	}
+
+}
